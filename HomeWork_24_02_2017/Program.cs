f0a2f671e1e1e3d4d26ffaf45e624bb537253453
Nikky1 <Nikky1@THE_ROOT>
2017-02-25 11:39:55 +0300

    Добавьте файлы проекта.

---
+++ HomeWork_24_02_2017/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+// Порезанов Николай.  Домашнее задание с первого занятия от 24.02.2017
+/*
+ * Написать программу“Анкета”. Последовательно задаютсявопросы(имя, фамилия, возраст,
+  рост, вес). В результате вся информация выводится в одну строчку.
+      а)используя склеивание;
+б) используя форматированный вывод.
+*/
+
+namespace HomeWork_24_02_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте! Вас приветствует программа -=Анкета=-. Нажмите Enter для продолжения.");
+            Console.ReadKey();
+            Console.WriteLine("Введите ваше имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Укажите ваш возраст (введите только числовое значение полных лет!)");
+            string age = Console.ReadLine();
+            Console.WriteLine("Укажите ваш рост (число в сантиметрах)");
+            string heigth = Console.ReadLine();
+            Console.WriteLine("Укажите ваш вес (число в килограммах)");
+            string weigth = Console.ReadLine();
+
+            Console.WriteLine("Результат А):"+name+" "+surname+", возраст "+age+", рост - "+heigth+" см, вес "+weigth+" кг." ); // Используеться склеивание при выводе результата
+            Console.WriteLine("Результат Б): {0} {1}, возраст {2}, рост - {3} см, вес {4} кг.", name, surname, age, heigth, weigth); // Используеться форматированный вывод.
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ TestList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+// Порезанов Николай. 
+// Вспомогательный лист для тестирование кусков кода. 
+
+namespace TestList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            Console.WriteLine("Vvedi x: "); // ввод Х
+            string str = Console.ReadLine();   // обьявлена переменная строковая str - ее значение считываеться с ввода. и записалась строка
+            x = Convert.ToDouble(str);      // конвертация циферка Х = строковой str, преобразование строки в циферку Х 
+            Console.WriteLine("Vvedi y: "); // ввод Y
+            y = Convert.ToDouble(Console.ReadLine());
+            double z = x + y;
+            Console.WriteLine("{0}+{1}={2}", x, y, z); // Вывод Х+У=ЗетЪ
+            Console.ReadKey();
+        }
+    }
+}
